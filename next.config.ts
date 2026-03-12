859317e9db0417cb0965a28e9b7d5697c28bd4f4
yoyodilip6@gmail.com <yoyodilip6@gmail.com>
2026-03-12 09:18:14 +0530

    Group of 3 and Group of 5 added

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
       },
     ],
   },
